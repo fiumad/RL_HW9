cbe33905db38b072ef4d03f278ca1ff2443966aa
Dan Fiumara <technodan143@gmail.com>
2024-11-24 17:12:06 -0500

    Delete extra train_buildings file and remove whitespace

--- startup_files/model.py
+++ startup_files/model.py
@@ -3,14 +3,14 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 class FC(nn.Module):
-    
+
     def __init__(self, in_dim, out_dim, num_hidden_layers, layer_size):
         super().__init__()
 
         self.num_layers = num_hidden_layers * 2 + 3 # *2 accounts for ReLU layers, +3 is input layer, input relu layer, output layer
 
         self.in_dim = in_dim
-        self.out_dim = out_dim        
+        self.out_dim = out_dim
 
         self.layer_size = layer_size
 
@@ -21,10 +21,10 @@ class FC(nn.Module):
 
         for i in range(1,self.num_hidden_layers):
             self.layer_list.append(nn.Linear(self.layer_size, self.layer_size))
-            
+
 
         self.layer_list.append(nn.Linear(self.layer_size, self.out_dim))
-        
+
     def forward(self, x):
 
         x = x.view(-1, self.in_dim)
@@ -35,23 +35,23 @@ class FC(nn.Module):
         return self.layer_list[self.num_hidden_layers](x)
 
 class CNN(nn.Module):
-    
+
     def __init__(self, in_dim, out_dim):
         super().__init__()
 
         self.in_dim = in_dim
-        self.out_dim = out_dim        
+        self.out_dim = out_dim
 
     def forward(self, x):
         pass
 
 class CNN_small(nn.Module):
-    
+
     def __init__(self, in_dim, out_dim):
         super().__init__()
 
         self.in_dim = in_dim
-        self.out_dim = out_dim        
+        self.out_dim = out_dim
 
         self.fc_layer_neurons = 200
 
@@ -65,7 +65,7 @@ class CNN_small(nn.Module):
         #1) padding is 0;
         #2) stride is picked such that the last step ends on the last pixel, i.e., padding is not used
         self.layer1_dim_h = (self.in_dim[1] - self.layer1_kernel_size[0]) / self.layer1_stride + 1
-        self.layer1_dim_w = (self.in_dim[2] - self.layer1_kernel_size[1]) / self.layer1_stride + 1        
+        self.layer1_dim_w = (self.in_dim[2] - self.layer1_kernel_size[1]) / self.layer1_stride + 1
 
         self.conv1 = nn.Conv2d(3, self.layer1_filters, self.layer1_kernel_size, stride=self.layer1_stride, padding=self.layer1_padding)
 
