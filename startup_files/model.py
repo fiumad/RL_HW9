1777bc922c5cc4d2ce64acd9a9857229fdf1b008
Dan Fiumara <technodan143@gmail.com>
2024-12-03 14:04:31 -0500

    reduce size of fully connected layers to lower parameter count to 4.5 mil

--- startup_files/model.py
+++ startup_files/model.py
@@ -38,51 +38,56 @@ class CNN(nn.Module):
 
     def __init__(self, in_dim, out_dim):
         super().__init__()
-
         self.in_dim = in_dim
         self.out_dim = out_dim
 
-        self.fc_layer_neurons = 10
+        # Convolutional layers
+        self.conv1 = nn.Conv2d(in_channels=3, out_channels=16, kernel_size=3, stride=1, padding=1)
+        self.conv2 = nn.Conv2d(in_channels=16, out_channels=32, kernel_size=3, stride=1, padding=1)
+        self.conv3 = nn.Conv2d(in_channels=32, out_channels=64, kernel_size=3, stride=1, padding=1)
 
-        # First Convolutional Layer
-        self.layer1_filters = 10
-        self.layer1_kernel_size = (4, 4)
-        self.layer1_stride = 1
-        self.layer1_padding = 0
+        # Batch normalization layers
+        self.bn1 = nn.BatchNorm2d(16)
+        self.bn2 = nn.BatchNorm2d(32)
+        self.bn3 = nn.BatchNorm2d(64)
 
-        #NB: these calculations assume:
-        #1) padding is 0;
-        #2) stride is picked such that the last step ends on the last pixel, i.e., padding is not used
-        self.layer1_dim_h = (self.in_dim[1] - self.layer1_kernel_size[0]) // self.layer1_stride + 1
-        self.layer1_dim_w = (self.in_dim[2] - self.layer1_kernel_size[1]) // self.layer1_stride + 1
-
-        self.conv1 = nn.Conv2d(3, self.layer1_filters, self.layer1_kernel_size, stride=self.layer1_stride, padding=self.layer1_padding)
-
-        
-
-        # Compute input size for fully connected layer
-        self.fc_inputs = int(self.layer1_filters * self.layer1_dim_h * self.layer1_dim_w)
+        # Max pooling layers
+        self.pool = nn.MaxPool2d(kernel_size=2, stride=2)
 
         # Fully connected layers
-        self.lin1 = nn.Linear(self.fc_inputs, self.fc_layer_neurons)
-        self.lin2 = nn.Linear(self.fc_layer_neurons, self.out_dim)
+        # Adjust input size of fc1 based on reduced feature map size
+        reduced_h = in_dim[1] // 8  # Spatial height after 3 pooling layers
+        reduced_w = in_dim[2] // 8  # Spatial width after 3 pooling layers
+        self.fc1 = nn.Linear(64 * reduced_h * reduced_w, 100)
+        self.fc2 = nn.Linear(100, 64)
+        self.fc3 = nn.Linear(64, out_dim)
+
+        # Dropout
+        self.dropout = nn.Dropout(0.5)
 
     def forward(self, x):
-        # Pass through convolutional layers
-        x = F.relu(self.conv1(x))
-        
-        # Print the shape before flattening
-        print(x.shape)
+        # Convolutional layers
+        x = F.relu(self.bn1(self.conv1(x)))
+        x = self.pool(x)
 
-        # Flatten the convolutional output
+        x = F.relu(self.bn2(self.conv2(x)))
+        x = self.pool(x)
+
+        x = F.relu(self.bn3(self.conv3(x)))
+        x = self.pool(x)
+
+        # Flatten for fully connected layers
         x = x.view(x.size(0), -1)
 
-        # Pass through fully connected layers
-        x = F.relu(self.lin1(x))
-        x = self.lin2(x)
-        return x
+        # Fully connected layers with dropout
+        x = F.relu(self.fc1(x))
+        x = self.dropout(x)
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
 
-class CNN_small(nn.Module):
+        return F.log_softmax(x, dim=1)
+
+        class CNN_small(nn.Module):
     def __init__(self, in_dim, out_dim):
         super().__init__()
 
